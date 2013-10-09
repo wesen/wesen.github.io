408d5a93560b0c4e8f6ed4aa2568a40eee6fc356
Manuel Odendahl <wesen@ruinwesen.com>
2013-10-09 22:44:25 +0200

    lab3 post

---
+++ lib/blog-from-r.r
@@ -0,0 +1,15 @@
+args <- commandArgs(trailingOnly = TRUE)
+
+myjekyllsite = c("/")
+
+KnitPost <- function(input, base.url = myjekyllsite) {
+    require(knitr)
+    opts_knit$set(base.url = base.url)
+    fig.path <- paste0("figs/", sub(".Rmd$", "", basename(input)), "/")
+    opts_chunk$set(fig.path = fig.path)
+    opts_chunk$set(fig.cap = "center")
+    render_jekyll()
+    knit(input, envir = parent.frame())
+}
+
+KnitPost(args[1])
